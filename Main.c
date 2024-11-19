9c3f27801b68258d95c97a157af3eb2c1b90fadd
emanameen9982773 <emanothard15@gmail.com>
2024-11-19 15:07:22 +0300

    almaha : implement writeAssignedApplicant()

--- Main.c
+++ Main.c
@@ -17,11 +17,26 @@ Applicant *list = NULL;
 int numOfApplicants = 0;
 void addApplicant(); // function prototypes
 void calculatePoints();
+void writeAssignedApplicant();
 
 int main()
 {
-
+/* FILE *file= fopen("Applicants.txt","r+");
+    fprintf(file,"The applicants are:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",1,"Fay","BSc",1,"CSC",4.5,"OWL");
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",2,"Norah","BSc",2,"Math",5.0,"OWL");
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",3,"Sarah","MSc",1,"Eng",5.0,"OWL");
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",4,"Ali","BSc",0,"CSC",3.4,"OWL");
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",5,"Ahmed","MSc",0,"SWE",4.9,"OWL");
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",6,"Maha","BSc",1,"InS",5.0,"OWL");
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",7,"Lama","BSc",0,"PYS",5.0,"OWL");*/
     addApplicant();
+
+writeAssignedApplicant();    
+    
+        /* code */
+    
+    
     
     for (int i = 0; i < numOfApplicants; i++) // print all the read applicant info *only for testing*
        printf("id: %d\nname: %s\neducation: %s\nexperience: %d\nmajor: %s\nGPA: %.2f\nstate: %s\npoint: %.2f\n\n********************\n", (list + i)->id, (list + i)->applicant, (list + i)->education, (list + i)->experience, (list + i)->major, (list + i)->GPA, (list + i)->state, (list + i)->points);
@@ -59,4 +74,20 @@ void calculatePoints()
         list[i].points = list[i].GPA + (float) list[i].experience;
         printf(" applicant #%d points are: %.2f \n and was calculated by %.2f + %d\n",i+1,list[i].points, list[i].GPA, list[i].experience); //only for testing
     }
+}
+
+
+void writeAssignedApplicant(){
+    FILE *file= fopen("Output_Applicants.txt","w+");
+    fprintf(file,"The applicants are:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");
+for ( int i = 0; i < numOfApplicants; i++){
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->experience,(list+i)->major,(list+i)->GPA,"OWL");
+}
+fprintf(file,"----------\n");
+fprintf(file,"The Candidates list is:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");
+
+for ( int i = 0; i < numOfApplicants; i++){
+    if(strcmp(list->state,"candidate"));
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->experience,(list+i)->major,(list+i)->GPA,(list+i)->state);
+}
 }
