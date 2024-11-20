546c3386ae740d243e49fb12165d8ece4cf3f747
AlmahaDevv <444200477@>
2024-11-20 14:24:39 +0300

    printlist() DONE ~
    writeAssignedApplicant() DONE~
    
    ALL ARE COMMENTED AS WELL ~~

--- Main.c
+++ Main.c
@@ -18,30 +18,24 @@ int numOfApplicants = 0;
 void addApplicant(); // function prototypes
 void calculatePoints();
 void writeAssignedApplicant();
+void printList();
 
 int main()
 {
-/* FILE *file= fopen("Applicants.txt","r+");
-    fprintf(file,"The applicants are:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",1,"Fay","BSc",1,"CSC",4.5,"OWL");
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",2,"Norah","BSc",2,"Math",5.0,"OWL");
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",3,"Sarah","MSc",1,"Eng",5.0,"OWL");
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",4,"Ali","BSc",0,"CSC",3.4,"OWL");
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",5,"Ahmed","MSc",0,"SWE",4.9,"OWL");
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",6,"Maha","BSc",1,"InS",5.0,"OWL");
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",7,"Lama","BSc",0,"PYS",5.0,"OWL");*/
-    addApplicant();
+
+   addApplicant();
 
 writeAssignedApplicant();    
     
         /* code */
     
     
-    
-    for (int i = 0; i < numOfApplicants; i++) // print all the read applicant info *only for testing*
-       printf("id: %d\nname: %s\neducation: %s\nexperience: %d\nmajor: %s\nGPA: %.2f\nstate: %s\npoint: %.2f\n\n********************\n", (list + i)->id, (list + i)->applicant, (list + i)->education, (list + i)->experience, (list + i)->major, (list + i)->GPA, (list + i)->state, (list + i)->points);
-    
+  //  for (int i = 0; i < numOfApplicants; i++) // print all the read applicant info *only for testing*
+       //printf("id: %d\nname: %s\neducation: %s\nexperience: %d\nmajor: %s\nGPA: %.2f\nstate: %s\npoint: %.2f\n\n********************\n", (list + i)->id, (list + i)->applicant, (list + i)->education, (list + i)->experience, (list + i)->major, (list + i)->GPA, (list + i)->state, (list + i)->points);
     calculatePoints();
+
+    printList();//testing yay
+
     return 0;
 }
 
@@ -77,17 +71,61 @@ void calculatePoints()
 }
 
 
-void writeAssignedApplicant(){
-    FILE *file= fopen("Output_Applicants.txt","w+");
-    fprintf(file,"The applicants are:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");
-for ( int i = 0; i < numOfApplicants; i++){
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->experience,(list+i)->major,(list+i)->GPA,"OWL");
-}
-fprintf(file,"----------\n");
-fprintf(file,"The Candidates list is:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");
+void writeAssignedApplicant(){// A func. outputs the file “Output_Applicants.txt”  with 3 lists ( Applicants & Candidates & Assigned ) 
 
-for ( int i = 0; i < numOfApplicants; i++){
-    if(strcmp(list->state,"candidate"));
-    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->experience,(list+i)->major,(list+i)->GPA,(list+i)->state);
-}
+    FILE *file= fopen("Output_Applicants.txt","w+");// create the required file 
+
+fprintf(file,"\nThe applicants are:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");//print the header & bar of the 1st list "Applicants"
+
+for ( int i = 0; i < numOfApplicants; i++){//Prints all Applicants with the OWL state 
+    fprintf(file,"%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->experience,(list+i)->major,(list+i)->GPA,"OWL");
+}//END OF FIRST LIST
+
+fprintf(file,"\n----------\n");//FORMATING ONLY
+fprintf(file,"\nThe Candidates list is:\n%-6s%-8s%-13s%-9s%-10s%-9s\n","id","Name","Education","Major","Points","State");//print the header & bar of the 2nd list "Candidates"
+
+
+for ( int i = 0; i < numOfApplicants; i++){//Prints all Applicants with the "Candidate" state only
+    if(strcmp(list->state,"candidate")== 0)
+        fprintf(file,"%-6d%-8s%-13s%-9s%-10.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->major,(list+i)->points,(list+i)->state);
+}//END OF SECOND LIST
+
+fprintf(file,"\n----------\n");//FORMATING ONLY
+fprintf(file,"\nThe Assigned Applicants:\n");//print the header of the 3rd list "Assigned"
+
+for ( int i = 0; i < numOfApplicants; i++){//Prints the Applicant with the "Assigned" state only
+    if(strcmp(list->state,"Assigned")== 0){
+        fprintf(file,"%-6d%-8s%-13s%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->major,(list+i)->points,(list+i)->state);
+    }
+        fprintf(file,"TEST\n");//remove later
+}//END OF THIRD LIST
+}//END OF THE FUNC. 
+
+void printList(){// A func. outputs the data of the list as ( Applicants & Candidates & Assigned ) ON SCREEN 
+
+printf("\nThe applicants are:\n%-6s%-8s%-13s%-14s%-9s%-7s%-9s\n","id","Name","Education","Experience","Major","GPA","State");//print the header & bar of the 1st list "Applicants"
+    for ( int i = 0; i < numOfApplicants; i++){//Prints all Applicants with the OWL state
+             printf("%-6d%-8s%-13s%-14d%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->experience,(list+i)->major,(list+i)->GPA,"OWL");//State as defaulte "OWL"
+    }//END OF FIRST LIST
+
+printf("\n----------\n");//FORMATING ONLY
+printf("\nThe Candidates are:\n%-6s%-8s%-13s%-9s%-10s%-9s\n","id","Name","Education","Major","Points","State");//print the header & bar of the 2nd list "Candidates"
+
+    for ( int i = 0; i < numOfApplicants; i++){//Prints all Applicants with the "Candidate" state only
+        if(strcmp(list->state,"candidate")== 0)
+             printf("%-6d%-8s%-13s%-9s%-10.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->major,(list+i)->points,(list+i)->state);
+    }//END OF SECOND LIST  
+
+      printf("\nTEST1\n");//remove later
+
+
+printf("\n----------\n");//FORMATING ONLY
+printf("\nThe Assigned Applicants:\n");//print the header of the 3rd list "Assigned"
+
+    for ( int i = 0; i < numOfApplicants; i++){//Prints the Applicant with the "Assigned" state only
+        if(strcmp(list->state,"Assigned")== 0)
+            printf("%-6d%-8s%-13s%-9s%-7.1f%-9s\n",(list+i)->id,(list+i)->applicant,(list+i)->education,(list+i)->major,(list+i)->points,(list+i)->state);
+    }//END OF THIRD LIST
+
+        printf("\nTEST2\n");//remove later
 }
